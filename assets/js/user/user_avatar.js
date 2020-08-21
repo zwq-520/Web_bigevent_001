6c3a61acd38a1ecb0113e8d9e6119a31826b3051
zwq-520 <742918439@qq.com>
2020-08-21 10:16:39 +0800

    个人中心功能完成

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -10,3 +10,56 @@ const options = {
 
 // 1.3 创建裁剪区域
 $image.cropper(options)
+
+// 上传文件
+$('#btnChooseImage').on('click', function () {
+    $('#file').click()
+})
+
+// 上传文件筐绑定 change 事件
+$('#file').on('change', function (e) {
+    // 获取到 上传的文件信息
+    var filelist = e.target.files
+    // 判断文件是否为空
+    if (filelist.length === 0) {
+        return layui.layer.msg('文件为空')
+    }
+    // 根据选择的文件创建一个 新的 url地址  然后把它赋值给裁剪区域的路径
+    // 拿到文件
+    var file = e.target.files[0]
+    //  利用 URL.createObjectURL  转为为路径
+    var newImgURL = URL.createObjectURL(file)
+    // 先`销毁`旧的裁剪区域，再`重新设置图片路径`，之后再`创建新的裁剪区域`：
+    $image
+        .cropper('destroy')      // 销毁旧的裁剪区域
+        .attr('src', newImgURL)  // 重新设置图片路径
+        .cropper(options)        // 重新初始化裁剪区域
+})
+
+// 点击确定 上传文件
+$('#btnUpload').on('click', function () {
+    //  拿到裁剪区域的图片 转化为base64的 图片（它是一个字符串）
+    var dataURL = $image
+        .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+            width: 100,
+            height: 100
+        })
+        .toDataURL('image/png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+    // 发送后台 进行页面渲染
+    $.ajax({
+        method: 'POST',
+        url: '/my/update/avatar',
+        data: {
+            avatar: dataURL
+        },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('上传失败')
+            }
+            layui.layer.msg('上传成功')
+            // 调用全局函数 重新获取数据渲染界面
+            window.parent.getUserInfo()
+        }
+
+    })
+})
